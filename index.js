9e7a42b686dcf658e88e2c2b6a1db12211367418
Hussein Talal <hossientalal2@gmail.com>
2020-05-15 13:02:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+const text = Document.getElementById('text');
+function manipulate(){
+  text.innerText = "This is really cool!";
+}
+Document.addEventListener("DOMContentLoaded", function(){
+  console.log("The DOM has loaded");
+  manipulate();
+});
